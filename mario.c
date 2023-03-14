7e1e070559a746b18ea8d0aa8b834b9f532559fa
enzololx <127688045+enzololx@users.noreply.github.com>
2023-03-14 23:58:16 +0000

    Tue, Mar 14, 2023, 11:58 PM +00:00

--- mario.c
+++ mario.c
@@ -2,13 +2,15 @@
 #include <cs50.h>
 int main(void)
 {
+    // asks size of grid
     int n;
     do
     {
         n = get_int("size: ");
     }
      while (n < 3);
-
+     
+    //prints de colums
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++ )
