fdb55695e90f5b7095443df4d5bd2ae24c6bc410
enzololx <127688045+enzololx@users.noreply.github.com>
2023-03-14 23:54:19 +0000

    Tue, Mar 14, 2023, 11:54 PM +00:00

--- mario.c
+++ mario.c
@@ -2,7 +2,13 @@
 #include <cs50.h>
 int main(void)
 {
-    int n = get_int ("size: ");
+    int n;
+    do
+    {
+        n = get_int("size: ");
+    }
+
+
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++ )
